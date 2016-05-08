756c1c1a1a1b1ed8feae8b38b7bdcf5c75fb3b42
rodrigo@rodrigovazquez.com.mx <rodrigo@rodrigovazquez.com.mx>
2016-05-08 14:53:28 -0600

    Se agregaron String resources, se agrego refactor en metodos CRUD en main activity

--- app/src/main/java/com/example/rodrigo/customcontentprovider/MainActivity.java
+++ app/src/main/java/com/example/rodrigo/customcontentprovider/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.rodrigo.customcontentprovider;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.net.Uri;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,8 +20,9 @@ import java.util.Arrays;
 //
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private EditText edtName, edtNickName;
-    private Button btnAddNew,btnDeleteAll,btnShowAll;
+    private Button btnAddNew, btnDeleteAll, btnShowAll;
 
 
     //Al cargar la actividad
@@ -28,59 +31,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtName = (EditText)findViewById(R.id.edtName);
-        edtNickName = (EditText)findViewById(R.id.edtNickName);
-
-        btnAddNew = (Button)findViewById(R.id.btnAddNew);
-        btnShowAll = (Button)findViewById(R.id.btnShowAll);
-        btnDeleteAll = (Button)findViewById(R.id.btnDeleteAll);
+        //Referenciamos los elementos de la vista
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtNickName = (EditText) findViewById(R.id.edtNickName);
+        btnAddNew = (Button) findViewById(R.id.btnAddNew);
+        btnShowAll = (Button) findViewById(R.id.btnShowAll);
+        btnDeleteAll = (Button) findViewById(R.id.btnDeleteAll);
 
         //Inserta un nuevo registro
         btnAddNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ContentValues contentValues = new ContentValues();
-                if(!edtName.getText().toString().isEmpty() && !edtNickName.getText().toString().isEmpty()){
+                if (!edtName.getText().toString().isEmpty() && !edtNickName.getText().toString().isEmpty()) {
 
-                    contentValues.put(CustomContentProvider.NAME,edtName.getText().toString());
-                    contentValues.put(CustomContentProvider.NICK_NAME,edtNickName.getText().toString());
+                    contentValues.put(CustomContentProvider.NAME, edtName.getText().toString());
+                    contentValues.put(CustomContentProvider.NICK_NAME, edtNickName.getText().toString());
+                    try {
+                        Uri uri = getContentResolver().insert(CustomContentProvider.CONTENT_URI, contentValues);
+                        Toast.makeText(MainActivity.this, "Record inserted", Toast.LENGTH_SHORT).show();
 
-                    Uri uri = getContentResolver().insert(CustomContentProvider.CONTENT_URI,contentValues);
-                    Toast.makeText(MainActivity.this, "Record inserted", Toast.LENGTH_SHORT).show();
-
-                    edtName.setText("");
-                    edtNickName.setText("");
-                }else{
+                        edtName.setText("");
+                        edtNickName.setText("");
+                    } catch (SQLException ex) {
+                        Log.e(TAG, ex.getMessage());
+                    }
+                } else {
                     Toast.makeText(MainActivity.this, "Please enter the records first", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-
+        //Muestra los registros existentes
         btnShowAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Consultamos los registros ordenados por nombre
                 String URL = "content://com.example.rodrigo.customcontentprovider.contentprovider/nicknames";
                 Uri friends = Uri.parse(URL);
-                Cursor cursor = getContentResolver().query(friends,null,null,null,"name");
 
-                String result = "Content providers results: ";
+                try {
+                    Cursor cursor = getContentResolver().query(friends, null, null, null, "name");
+                    String result = "Content providers results: ";
 
-                if(!cursor.moveToFirst()){
-                    Toast.makeText(MainActivity.this, "No content yet", Toast.LENGTH_SHORT).show();
-                }else{
-                    do {
-                        result = result + "\n"
-                                 + cursor.getString(cursor.getColumnIndex(CustomContentProvider.NAME))
-                                 + " has nickname: "
-                                 + cursor.getString(cursor.getColumnIndex(CustomContentProvider.NICK_NAME));
-                    }while (cursor.moveToNext());
-                    if(!result.isEmpty()){
-                        Toast.makeText(MainActivity.this, "" + result, Toast.LENGTH_LONG).show();
-                    }else{
-                        Toast.makeText(MainActivity.this, "No records present", Toast.LENGTH_SHORT).show();
+                    if (!cursor.moveToFirst()) {
+                        Toast.makeText(MainActivity.this, "No content yet", Toast.LENGTH_SHORT).show();
+                    } else {
+                        do {
+                            result = result + "\n"
+                                    + cursor.getString(cursor.getColumnIndex(CustomContentProvider.NAME))
+                                    + " has nickname: "
+                                    + cursor.getString(cursor.getColumnIndex(CustomContentProvider.NICK_NAME));
+                        } while (cursor.moveToNext());
+                        if (!result.isEmpty()) {
+                            Toast.makeText(MainActivity.this, "" + result, Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "No records present", Toast.LENGTH_SHORT).show();
+                        }
                     }
+                } catch (IllegalArgumentException ex) {
+                    Log.e(TAG, ex.getMessage());
+                }
+            }
+        });
+
+        //Borra los registros existentes
+        btnDeleteAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String URL = "content://com.example.rodrigo.customcontentprovider.contentprovider/nicknames";
+                Uri friends = Uri.parse(URL);
+                try {
+                    int count = getContentResolver().delete(friends, null, null);
+                    String countNum = count + " records are deleted ";
+                    Toast.makeText(MainActivity.this, "" + countNum, Toast.LENGTH_SHORT).show();
+                } catch (IllegalArgumentException ex) {
+                    Log.e(TAG, ex.getMessage());
                 }
             }
         });
